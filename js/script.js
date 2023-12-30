c69ae0a4b715f64fa1ffbe1192471e03fa571818
Humberto Galdino <humberto.galdino@live.com>
2023-12-30 15:57:07 -0300

    Refatorando com ESModules

---
+++ js/script.js
@@ -0,0 +1,30 @@
+import { Modal } from './modal.js';
+
+// Variáveis
+const form = document.querySelector("form");
+const inputWeight = document.querySelector("#weight");
+const inputHeight = document.querySelector("#height");
+
+
+// Eventos
+form.onsubmit = handleSubmit;
+
+// Funções
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const weight = inputWeight.value;
+    const height = inputHeight.value;
+
+    let result = IMC(weight, height);
+
+    Modal.message.innerText = `Seu IMC é de ${result}!`;
+
+    Modal.open();
+};
+
+function IMC(weight, height){
+    return (weight / ((height / 100) ** 2)).toFixed(2);
+}
+
+
